dcae63d0d7bf6ffff16ffdb057609b84abc77d5b
charuka deshan <itscharukadeshan@gmail.com>
2024-06-13 11:45:10 +0530

    config🔧: Update the port

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = 5172;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
