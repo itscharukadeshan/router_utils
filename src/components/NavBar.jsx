5a2827ed6dd4581872b8f759bc92d743c478f09e
charuka deshan <itscharukadeshan@gmail.com>
2024-08-16 17:55:38 +0530

    docs📝: remove unwanted comment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ function NavBar() {
     const intervalId = setInterval(checkBrowserlessStatus, 3000);
 
     return () => clearInterval(intervalId);
-  }, []); // Remove browserlessStatus from the dependency array
+  }, []);
 
   return (
     <div className='navbar bg-blue-950 shadow-lg'>
