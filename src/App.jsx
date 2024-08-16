6469d123622e6739772379e20fd902050522cd71
charuka deshan <itscharukadeshan@gmail.com>
2024-08-16 17:15:28 +0530

    feat✨: Add time, status, signal to dynamically update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@
 
 import "./index.css";
 import NavBar from "./components/NavBar";
-import { HiStatusOnline } from "react-icons/hi";
-import { MdSignalWifi1Bar } from "react-icons/md";
+import HutchCard from "./components/HutchCard";
+import DialogCard from "./components/DialogCard";
 
 function App() {
-  // TODO Fix env and add BASE_URL to the browserless status --------------- {{Priority 01}}
   // TODO Refactor card to be separate components (dynamically pass data).
   // TODO Implement the table with status and other data.
   // TODO Add Legacy restart
@@ -16,80 +15,8 @@ function App() {
     <>
       <NavBar />
       <div className='flex flex-col lg:flex-row w-full'>
-        <div className='card lg:card-normal bg-pink-950 shadow-2xl flex-1 m-6 ml-6'>
-          <div className='card-title mt-6 mx-auto font-serif font-extrabold text-warning '>
-            Dialog
-          </div>
-          <div className='card-body'>
-            <button className='btn btn-sm btn-ghost btn-outline w-fit mx-auto '>
-              Reboot Dialog
-            </button>
-            <div className='stats shadow stats-vertical bg-transparent my-6 lg:w-2/4 lg:mx-auto '>
-              <div className='stat place-items-center'>
-                <div className='stat-title text-gray-400 font-mono mb-2'>
-                  Signal
-                </div>
-                <div className='stat-value m-2 '>
-                  <MdSignalWifi1Bar />
-                </div>
-              </div>
-
-              <div className='stat place-items-center'>
-                <div className='stat-title text-gray-400 font-mono mb-2'>
-                  Sim status
-                </div>
-                <div className='stat-value m-2'>
-                  <HiStatusOnline />
-                </div>
-              </div>
-
-              <div className='stat place-items-center'>
-                <div className='stat-title text-gray-400 font-mono mb-2'>
-                  Uptime
-                </div>
-                <div className='stat-value '>1h 30m</div>
-              </div>
-            </div>
-            <div className='overflow-x-auto'></div>
-          </div>
-        </div>
-        <div className='card lg:card-normal bg-orange-950  shadow-2xl flex-1 m-6 ml-6'>
-          <div className='card-title mt-6 mx-auto font-serif font-extrabold text-warning'>
-            Hutch
-          </div>
-          <div className='card-body'>
-            <button className='btn btn-sm btn-ghost btn-outline  w-fit mx-auto'>
-              Reboot Hutch
-            </button>
-            <div className='stats shadow stats-vertical bg-transparent my-6 lg:w-2/4 lg:mx-auto '>
-              <div className='stat place-items-center'>
-                <div className='stat-title text-gray-400 font-mono mb-2'>
-                  Signal
-                </div>
-                <div className='stat-value m-2 '>
-                  <MdSignalWifi1Bar />
-                </div>
-              </div>
-
-              <div className='stat place-items-center'>
-                <div className='stat-title text-gray-400 font-mono mb-2'>
-                  Sim status
-                </div>
-                <div className='stat-value m-2'>
-                  <HiStatusOnline />
-                </div>
-              </div>
-
-              <div className='stat place-items-center'>
-                <div className='stat-title text-gray-400 font-mono mb-2'>
-                  Uptime
-                </div>
-                <div className='stat-value '>1h 30m</div>
-              </div>
-            </div>
-            <div className='overflow-x-auto'></div>
-          </div>
-        </div>
+        <DialogCard />
+        <HutchCard />
       </div>
     </>
   );
---
+++ src/components/HutchCard.jsx
@@ -0,0 +1,137 @@
+/** @format */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { HiStatusOnline, HiStatusOffline } from "react-icons/hi";
+import {
+  MdSignalWifiStatusbarNull,
+  MdSignalWifiStatusbar1Bar,
+  MdSignalWifiStatusbar2Bar,
+  MdSignalWifiStatusbar3Bar,
+  MdSignalWifiStatusbar4Bar,
+} from "react-icons/md";
+import { API_URL } from "../config";
+import moment from "moment";
+
+function HutchCard() {
+  const hutch_stats_url = `${API_URL}/api/status/hutch`;
+  const hutch_reboot = `${API_URL}/api/restart_hutch`;
+  const hutch_legacy_reboot = `${API_URL}/api/legacy/restart_hutch`;
+
+  const [data, setData] = useState({
+    service_status: "Loading ...",
+    sim_status: "Loading ...",
+    plmn: "Unknown Network",
+    rsrp: "Loading ...",
+    rsrq: "Loading ...",
+    uplink_rate: "Loading ...",
+    downlink_rate: "Loading ...",
+    uplink_traffic: "Loading ...",
+    downlink_traffic: "Loading ...",
+    web_signal: "Loading ...",
+    online_time: "Loading ...",
+  });
+
+  const formatUptime = (seconds) => {
+    return moment.duration(seconds, "seconds").humanize();
+  };
+
+  const getSignalIcon = (strength) => {
+    switch (parseInt(strength)) {
+      case 0:
+        return MdSignalWifiStatusbarNull;
+      case 1:
+        return MdSignalWifiStatusbar1Bar;
+      case 2:
+        return MdSignalWifiStatusbar2Bar;
+      case 3:
+        return MdSignalWifiStatusbar3Bar;
+      case 4:
+        return MdSignalWifiStatusbar4Bar;
+      default:
+        return MdSignalWifiStatusbarNull;
+    }
+  };
+
+  const getSimStatusIcon = (status) => {
+    return status.toLowerCase() === "normal" ? HiStatusOnline : HiStatusOffline;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(hutch_stats_url);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [hutch_stats_url]);
+
+  const handleButtonClick = async (endpoint) => {
+    try {
+      await axios.get(endpoint);
+      if (endpoint === hutch_reboot) {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await axios.get(endpoint);
+      }
+    } catch (error) {
+      console.error("Unable to restart");
+    }
+  };
+
+  return (
+    <div className='card lg:card-normal bg-pink-950 shadow-2xl flex-1 m-6 ml-6'>
+      <div className='card-title text-3xl mt-6 mx-auto font-serif font-extrabold text-warning '>
+        Hutch
+      </div>
+      <div className='card-body m-auto'>
+        <div className='flex flex-col lg:flex-row '>
+          <button
+            onClick={() => handleButtonClick(hutch_reboot)}
+            className='btn btn-sm btn-outline btn-warning w-fit m-auto my-2 lg:mx-2'>
+            Reboot Hutch
+          </button>
+          <button
+            onClick={() => handleButtonClick(hutch_legacy_reboot)}
+            className='btn btn-sm btn-outline btn-warning w-fit m-auto my-2 lg:mx-2 '>
+            Reboot Hutch - Legacy
+          </button>
+        </div>
+        <div className='stats shadow stats-vertical bg-transparent my-6 lg:w-fit lg:mx-auto '>
+          <div className='stat place-items-center'>
+            <div className='stat-title text-gray-400 font-bold font-mono mb-2'>
+              Signal ({data.web_signal})
+            </div>
+            <div className='stat-value m-2 '>
+              {React.createElement(getSignalIcon(data.web_signal))}
+            </div>
+          </div>
+
+          <div className='stat place-items-center'>
+            <div className='stat-title text-gray-400 font-bold font-mono mb-2'>
+              Sim status
+            </div>
+            <div className='stat-value m-2'>
+              {React.createElement(getSimStatusIcon(data.sim_status))}
+            </div>
+          </div>
+
+          <div className='stat place-items-center'>
+            <div className='stat-title text-gray-400 font-mono mb-2'>
+              Uptime
+            </div>
+            <div className='stat-value '>{formatUptime(data.online_time)}</div>
+          </div>
+        </div>
+        <div className='overflow-x-auto'></div>
+      </div>
+    </div>
+  );
+}
+
+export default HutchCard;
